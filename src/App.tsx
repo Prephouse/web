45924b704c694313bec7b1e233b992f00b1fedb6
Jadon Fan <jdsare@gmail.com>
2021-11-11 02:17:00 -0500

    resolve merge conflicts; refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-=======
-import { useEffect, useState } from "react";
-import { IntlProvider } from "react-intl";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
-import usePrevious from "./hooks/usePrevious";
-import { RootState } from "./store/rootReducer";
+import { useEffect, useState } from 'react';
+import { IntlProvider } from 'react-intl';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import usePrevious from './hooks/usePrevious';
+import { RootState } from './store/rootReducer';
 import {
   ABOUT_PATH,
   HOME_PATH,
   PRACTICE_PATH,
   COMPARE_PATH,
   TIPS_PATH,
-  USER_REGISTRATION_PATH, DASHBOARD_PATH
-} from "./strings/paths";
-import strings from "./strings/strings";
-import generateTheme from "./styles/themes";
-import { CssBaseline, responsiveFontSizes, ThemeProvider } from "@mui/material";
-import Home from "./components/home/Home";
-import ActionBar from "./components/actionbar/ActionBar";
-import About from "./components/about/About";
-import Footer from "./components/footer/Footer";
-import RegistrationForm from "./components/user/registration/RegistrationForm";
-import PageNotFound from "./components/error/PageNotFound";
-import PracticeGround from "./components/practice/PracticeGround";
-import TipBook from "./components/tips/TipBook";
-import CompareBoard from "./compare/CompareBoard";
-import Dashboard from "./components/dashboard/Dashboard";
+  USER_REGISTRATION_PATH,
+  DASHBOARD_PATH,
+} from './strings/paths';
+import strings from './strings/strings';
+import generateTheme from './styles/themes';
+import { CssBaseline, responsiveFontSizes, ThemeProvider } from '@mui/material';
+import Home from './components/home/Home';
+import ActionBar from './components/actionbar/ActionBar';
+import About from './components/about/About';
+import Footer from './components/footer/Footer';
+import RegistrationForm from './components/user/registration/RegistrationForm';
+import PageNotFound from './components/error/PageNotFound';
+import PracticeGround from './components/practice/PracticeGround';
+import TipBook from './components/tips/TipBook';
+import CompareBoard from './compare/CompareBoard';
+import Dashboard from './components/dashboard/Dashboard';
 
 const App = () => {
   const prefersDarkMode: boolean = useSelector((state: RootState) => state.settingsReducer.prefersDarkMode);
@@ -45,22 +41,21 @@ const App = () => {
   const prevPref = usePrevious(prefersDarkMode);
   useEffect(() => {
     if (prevPref !== prefersDarkMode) {
-      console.log("PREFERS", prefersDarkMode);
-      setTheme((establishTheme()));
+      console.log('PREFERS', prefersDarkMode);
+      setTheme(establishTheme());
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [prefersDarkMode, prevPref])
->>>>>>> 78e5916 (implement)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [prefersDarkMode, prevPref]);
 
   return (
     <BrowserRouter>
       <IntlProvider
         locale="en-US"
         defaultLocale="en-US"
-        messages={strings["en-US"]}
+        messages={strings['en-US']}
         onError={err => {
-          if (err.code === "MISSING_TRANSLATION") {
-            console.warn("Missing translation", err.message);
+          if (err.code === 'MISSING_TRANSLATION') {
+            console.warn('Missing translation', err.message);
             return;
           }
           throw err;
@@ -88,6 +83,6 @@ const App = () => {
       </IntlProvider>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
