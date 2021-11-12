436a5392055fba349f38a460ab61c987108f3e33
Jadon Fan <jdsare@gmail.com>
2021-11-12 14:55:29 -0500

    integrate i18n linter

---
+++ .eslintrc.i18n.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['plugin:i18n-json/recommended'],
+  rules: {
+    'i18n-json/valid-message-syntax': 2,
+    'i18n-json/valid-json': 2,
+    'i18n-json/sorted-keys': 0,
+  },
+};
