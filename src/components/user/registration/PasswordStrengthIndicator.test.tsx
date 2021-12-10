d5c3d869a5bf4828593d02c43d4d7fd1bdd1ef91
Jadon Fan <jdsare@gmail.com>
2021-12-09 22:00:54 -0500

    update dependencies

---
+++ src/components/user/registration/PasswordStrengthIndicator.test.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { IntlProvider } from 'react-intl';
+import PasswordStrengthMeter from './PasswordStrengthIndicator';
+import strings from '../../../strings/strings';
+
+const locale = 'en-US';
+let wrapper: ReturnType<typeof mount>;
+
+const initPsmComponent = function initPsmComponent(password: string) {
+  wrapper = mount(
+    <IntlProvider locale={locale} messages={strings[locale]}>
+      <PasswordStrengthMeter password={password} />
+    </IntlProvider>
+  );
+};
+
+const matchMeterText = function matchMeterText(expected: string) {
+  expect(wrapper.find('#password-strength').text()).toBe(expected);
+};
+
+describe('Password Strength Indicator', () => {
+  // eslint-disable-next-line jest/expect-expect
+  it('indicate extremely weak password (0)', () => {
+    initPsmComponent('a');
+    matchMeterText('Extremely Weak');
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('indicate very weak password (1)', () => {
+    initPsmComponent('sdffa');
+    matchMeterText('Very Weak');
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('indicate weak password (2)', () => {
+    initPsmComponent('sd234ffa');
+    matchMeterText('Weak');
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('indicate adequate password (3)', () => {
+    initPsmComponent('sd234ff313');
+    matchMeterText('Adequate');
+  });
+
+  // eslint-disable-next-line jest/expect-expect
+  it('indicate strong password (4)', () => {
+    initPsmComponent('sd234ff31332a');
+    matchMeterText('Strong');
+  });
+});
