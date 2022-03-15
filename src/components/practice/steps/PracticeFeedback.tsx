1452b2cfe3ad470c66347c85f204946853a482a6
Chandler Lei <chandlerlei87@gmail.com>
2022-03-15 16:41:16 -0400

    Practice end page

--- src/components/practice/steps/PracticeFeedback.tsx
+++ src/components/practice/steps/PracticeFeedback.tsx
@@ -1,3 +1,32 @@
-const PracticeFeedback = () => null;
+import { useIntl } from 'react-intl';
+import { useNavigate } from 'react-router-dom';
+
+import { Button, ButtonGroup, Stack, Typography } from '@mui/material';
+
+import { PROGRESS_PATH } from 'strings/paths';
+
+const PracticeFeedback = () => {
+  const intl = useIntl();
+  const navigate = useNavigate();
+
+  return (
+    <ButtonGroup
+      fullWidth
+      sx={{
+        '& > *': { height: 496 },
+        '& .MuiButton-root': { textTransform: 'none' },
+      }}
+    >
+      <Button onClick={() => navigate(PROGRESS_PATH)}>
+        <Stack>
+          <Typography sx={{ fontSize: 60, mb: 4 }}>
+            {intl.formatMessage({ id: 'practice.end.interview' })}
+          </Typography>
+          <Typography>{intl.formatMessage({ id: 'practice.end.description' })}</Typography>
+        </Stack>
+      </Button>
+    </ButtonGroup>
+  );
+};
 
 export default PracticeFeedback;
